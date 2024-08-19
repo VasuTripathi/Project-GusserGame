8c2a6aec70e595497f23e79e23a1ae083af477b4
Vasu Tripathi <tirpathivasu7@gmail.com>
2024-08-19 20:51:03 +0530

    update

--- src/GuesserGame/MyGuesserGame.java
+++ src/GuesserGame/MyGuesserGame.java
@@ -27,19 +27,22 @@ class Umpire{
 
     public void CollectNumForGuesser(){
         Guesser Gu = new Guesser();
-        Guessernum = Gu.GuesserNumb();
+        Guessernum = Gu.GuesserNumber();
     }
     public void CollectNumForPlayers() {
         Player P1 = new Player();
         System.out.println("Player1 kindly guess a number");
+        Playernum1 = P1.PlayerNumb();
+
         Player P2 = new Player();
         System.out.println("Player2 kindly guess a number");
+        Playernum2 = P2.PlayerNumb();
+
         Player P3 = new Player();
         System.out.println("Player3 kindly guess a number");
-
-        Playernum1 = P1.PlayerNumb();
-        Playernum2 = P2.PlayerNumb();
         Playernum3 = P3.PlayerNumb();
+
+
     }
     public void Compare(){
      if(Guessernum == Playernum1){
@@ -74,6 +77,11 @@ class Umpire{
 
 public class MyGuesserGame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("_-_-_-_-_GAME STARTED_-_-_-_-_-");
+        Umpire UM = new Umpire();
+        UM.CollectNumForGuesser();
+        UM.CollectNumForPlayers();
+        UM.Compare();
+        System.out.println("_-_-_-_-_GAME OVER_-_-_-_-_-");
     }
 }
